8472ea2d9631d438a1d135c48115f66ba28f1df4
amribrahim <amr.ibrahem.shaker@gmail.com>
2018-06-16 12:05:54 -0500

    test js

--- try.js
+++ try.js
@@ -2,3 +2,6 @@ setTimeout(function () {console.log("world");}, 2000);
 console.log("hello");
 setTimeout(function () {console.log("world");}, 3000);
 console.log("ereeee . hello");
+
+setTimeout(function () {console.log("world");}, 3000);
+console.log("ereeee . hello");
